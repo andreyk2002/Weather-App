449e2afe900ba4e6f47a84fb2301f25b126bdef2
Andrey Kuksa <andrey20011005@gmail.com>
2021-04-16 21:02:51 -0700

    Delete comments

--- Task2Lab7/AppDelegate.m
+++ Task2Lab7/AppDelegate.m
@@ -1,10 +1,4 @@
-//
-//  AppDelegate.m
-//  Task2Lab7
-//
-//  Created by Ivan on 17.04.2020.
-//  Copyright © 2020 Ivan. All rights reserved.
-//
+
 
 #import "AppDelegate.h"
 
--- Task2Lab7/SceneDelegate.h
+++ Task2Lab7/SceneDelegate.h
@@ -1,11 +1,4 @@
 //
-//  SceneDelegate.h
-//  Task2Lab7
-//
-//  Created by Ivan on 17.04.2020.
-//  Copyright © 2020 Ivan. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
